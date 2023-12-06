63d824a3bf21466c714ef5ff4fa8f862a079424a
dpark19 <d.park.official@gmail.com>
2023-12-06 12:47:20 -0500

    knexfile setup

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: 'database',
       password: 'docker',
       user: 'postgres',
       port: 5432,
